cd0d17acc519d99f3c3ff7ddfa622d6a68f1378c
Garrett Ourth <garrettourth@gmail.com>
2023-06-20 21:26:47 -0500

    add dynamic nav

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -25,9 +26,10 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(require("./routes/static"))
 
 // Index route
-app.get("/", function(req,res){
-  res.render("index", {title: "Home"})
-})
+// app.get("/", function(req,res){
+//   res.render("index", {title: "Home"})
+// })
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
